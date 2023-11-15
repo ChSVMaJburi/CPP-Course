6f3c1985a78b9a2a710a9501293fa856fc46155d
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-15 22:16:28 +0300

    sol of spiral

--- cpp-23-24-2d-spiral/cpp-23-24-2d-spiral.hpp
+++ cpp-23-24-2d-spiral/cpp-23-24-2d-spiral.hpp
@@ -7,10 +7,10 @@ int** MakeSpiral(int n) {
   for (int it = 0; it < (n + 1) / 2; ++it) {
 	int l = it;
 	int r = n - it - 1;
-	for (int i = l; i < r; ++i) {
+	for (int i = l; i < r + (l == r); ++i) {
 	  array[l][i] = ++counter;
 	}
-	for (int i = l; i < r + (l == r); ++i) {
+	for (int i = l; i < r; ++i) {
 	  array[i][r] = ++counter;
 	}
 	for (int i = r; i > l; --i) {
