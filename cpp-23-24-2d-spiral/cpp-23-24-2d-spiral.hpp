f59350bc9b97179c2e0a87b10999609acb89b7eb
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-15 22:22:29 +0300

    sol of spiral

--- cpp-23-24-2d-spiral/cpp-23-24-2d-spiral.hpp
+++ cpp-23-24-2d-spiral/cpp-23-24-2d-spiral.hpp
@@ -1,24 +1,24 @@
 int** MakeSpiral(int n) {
-  int** array = new int*[n];
-  for (int i = 0; i < n; ++i) {
+ int** array = new int*[n];
+ for (int i = 0; i < n; ++i) {
 	array[i] = new int[n];
-  }
-  int counter = 0;
-  for (int it = 0; it < (n + 1) / 2; ++it) {
+ }
+ int counter = 0;
+ for (int it = 0; it < (n + 1) / 2; ++it) {
 	int l = it;
 	int r = n - it - 1;
 	for (int i = l; i < r + (l == r); ++i) {
-	  array[l][i] = ++counter;
+	 array[l][i] = ++counter;
 	}
 	for (int i = l; i < r; ++i) {
-	  array[i][r] = ++counter;
+	 array[i][r] = ++counter;
 	}
 	for (int i = r; i > l; --i) {
-	  array[r][i] = ++counter;
+	 array[r][i] = ++counter;
 	}
 	for (int i = r; i > l; --i) {
-	  array[i][l] = ++counter;
+	 array[i][l] = ++counter;
 	}
-  }
-  return array;
+ }
+ return array;
 }
