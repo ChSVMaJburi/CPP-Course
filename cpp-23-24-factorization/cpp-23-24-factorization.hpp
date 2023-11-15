dcbdab24a06ee68bdaf48d30f3558f7d15e7eae3
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-16 00:12:59 +0300

    solve of factorization

--- cpp-23-24-factorization/cpp-23-24-factorization.hpp
+++ cpp-23-24-factorization/cpp-23-24-factorization.hpp
@@ -1,7 +1,16 @@
 #pragma once
 #include <vector>
 
-std::vector<int> Factorize(int /*n*/) {
-    /* Not Implemented */
-    return {};
+std::vector<int> Factorize(int n) {
+    std::vector<int> answer;
+    for (int i = 2; i * i <= n; ++i) {
+      while (n % i == 0) {
+        answer.push_back(i);
+        n /= i;
+      }
+    }
+    if (n != 1) {
+      answer.push_back(n);
+    }
+    return answer;
 }
