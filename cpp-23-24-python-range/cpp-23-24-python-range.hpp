7753631c51446c46a9f950e2392c7307d73c9fc2
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-15 23:28:02 +0300

    solve of python

--- cpp-23-24-python-range/cpp-23-24-python-range.hpp
+++ cpp-23-24-python-range/cpp-23-24-python-range.hpp
@@ -1,5 +1,11 @@
 #include <utility>
 
 std::pair<int, const int*> Range(int from, int to, int step = 1) {
-
+  int sz = (to - from + step - 1) / step;
+  int* answer = new int[sz];
+  int counter = 0;
+  for (int i = from; i < to; i += step) {
+    answer[counter++] = i;
+  }
+  return {sz, answer};
 }
