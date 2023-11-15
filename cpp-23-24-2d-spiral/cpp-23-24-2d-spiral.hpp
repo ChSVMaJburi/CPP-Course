6d3a5c9b30494cb1ca4da78c77d5f8ab30915bf3
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-15 22:06:00 +0300

    sol of spiral

--- cpp-23-24-2d-spiral/cpp-23-24-2d-spiral.hpp
+++ cpp-23-24-2d-spiral/cpp-23-24-2d-spiral.hpp
@@ -1,24 +1,24 @@
 int** MakeSpiral(int n) {
-	int **array = new int* [n];
-	for (int i = 0; i < n; ++i) {
-		array[i] = new int [n];
+  int **array = new int* [n];
+  for (int i = 0; i < n; ++i) {
+	array[i] = new int [n];
+  }
+  int counter = 0;
+  for (int it = 0; it < (n + 1) / 2; ++it) {
+	int l = it;
+	int r = n - it - 1;
+	for (int i = l; i < r + (l == r); ++i) {
+	  array[l][i] = ++counter;
 	}
-	int counter = 0;
-	for (int it = 0; it < (n + 1) / 2; ++it) {
-		int l = it;
-		int r = n - it - 1;
-		for (int i = l; i < r + (l == r); ++i) {
-			array[l][i] = ++counter;
-		}
-		for (int i = l; i < r; ++i) {
-			array[i][r] = ++counter;
-		}
-		for (int i = r; i > l; --i) {
-			array[r][i] = ++counter;
-		}
-		for (int i = r; i > l; --i) {
-			array[i][l] = ++counter;
-		}
+	for (int i = l; i < r; ++i) {
+	  array[i][r] = ++counter;
 	}
-	return array;
+	for (int i = r; i > l; --i) {
+	  array[r][i] = ++counter;
+	}
+	for (int i = r; i > l; --i) {
+	  array[i][l] = ++counter;
+	}
+  }
+  return array;
 }
