859a56f0ab98f8dfcba6e22f1e682a492dd14c50
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-16 23:43:48 +0300

    solve of ring buffer

--- cpp-23-24-ring-buffer/cpp-23-24-ring-buffer.hpp
+++ cpp-23-24-ring-buffer/cpp-23-24-ring-buffer.hpp
@@ -1,25 +1,38 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
 
 class RingBuffer {
  public:
-  explicit RingBuffer(size_t capacity) {
-  }
+  explicit RingBuffer(size_t capacity) : left_(0), size_(0), arr_(capacity) {}
 
-  size_t Size() const {
-    return 0;
-  }
+  size_t Size() const { return size_; }
 
-  bool Empty() const {
-    return false;
-  }
+  bool Empty() const { return size_ != 0; }
 
   bool TryPush(int element) {
+    if (Size() == arr_.size()) {
+      return false;
+    }
+    std::size_t right = (left_ + size_) % arr_.size();
+    arr_[right] = element;
+    ++size_;
     return true;
   }
 
   bool TryPop(int* element) {
+    if (Empty()) {
+      return false;
+    }
+    *element = arr_[left_];
+    ++left_;
+    --size_;
     return true;
   }
+
+ private:
+  std::vector<int> arr_;
+  std::size_t left_;
+  std::size_t size_;
 };
