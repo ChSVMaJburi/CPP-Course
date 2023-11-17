7da37538d9362b11d329102944ea508f97efb0c2
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-17 21:46:22 +0300

    solve of spiral

--- cpp-23-24-2d-spiral/cpp-23-24-2d-spiral.hpp
+++ cpp-23-24-2d-spiral/cpp-23-24-2d-spiral.hpp
@@ -1,6 +1,6 @@
 int** MakeSpiral(unsigned int n) {
   int** array = new int*[n];
-  for (int i = 0; i < n; ++i) {
+  for (unsigned int i = 0; i < n; ++i) {
     array[i] = new int[n];
   }
   unsigned int counter = 0;
