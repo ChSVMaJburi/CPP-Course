7130f42d6e3263f678a2ebc5098f5e45f9c0d701
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-17 22:01:30 +0300

    solve of pontwise sum

--- cpp-23-24-pointwise-sum/cpp-23-24-pointwise-sum.hpp
+++ cpp-23-24-pointwise-sum/cpp-23-24-pointwise-sum.hpp
@@ -1,6 +1,6 @@
 int* PointwiseSum(const int* a, const int* b, int n) {
   int* answer = new int[n];
-  for (int i = 0; i < n; ++i) {
+  for (unsigned int i = 0; i < n; ++i) {
     answer[i] = a[i] + b[i];
   }
   return answer;
