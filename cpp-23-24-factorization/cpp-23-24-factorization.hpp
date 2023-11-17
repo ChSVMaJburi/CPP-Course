4da4984f47865251a3eb45028dd3c40a02f1dc9a
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-17 22:03:08 +0300

    solve of factorization

--- cpp-23-24-factorization/cpp-23-24-factorization.hpp
+++ cpp-23-24-factorization/cpp-23-24-factorization.hpp
@@ -3,7 +3,7 @@
 
 std::vector<int> Factorize(int n) {
   std::vector<int> answer;
-  for (int i = 2; i * i <= n; ++i) {
+  for (unsigned int i = 2; i * i <= n; ++i) {
     while (n % i == 0) {
       answer.push_back(i);
       n /= i;
