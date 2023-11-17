6f019a47f62d14924fffbf62ba08f1fa2ecbab81
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-17 21:53:49 +0300

    solve of python range

--- cpp-23-24-python-range/cpp-23-24-python-range.hpp
+++ cpp-23-24-python-range/cpp-23-24-python-range.hpp
@@ -2,12 +2,12 @@
 
 int Abs(int num) { return (num > 0 ? num : -num); }
 
-std::pair<int, const int *> Range(int from, int to, int step = 1) {
+std::pair<int, const int*> Range(int from, int to, int step = 1) {
   if (from <= to && step <= 0 || from > to && step >= 0) {
     return {};
   }
   unsigned int sz = (Abs(to - from) + Abs(step) - 1) / Abs(step);
-  int *answer = new int[sz];
+  int* answer = new int[sz];
   unsigned int counter = 0;
   for (int i = from; counter < sz; i += step) {
     answer[counter++] = i;
