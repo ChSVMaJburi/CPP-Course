06cb620bacebeead44b3fc54575d2749bccac476
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-11-29 14:27:26 +0300

    solution of string maybe

--- cpp-23-24-string/cpp-23-24-string.hpp
+++ cpp-23-24-string/cpp-23-24-string.hpp
@@ -1,5 +1,52 @@
 #pragma once
 
-class String {
+#include <iostream>
+#include <vector>
+#include <cstring>
 
+class String {
+ private:
+  void SetCapacity(size_t new_cap);
+  char* str_;
+  size_t capacity_;
+  size_t size_;
+ public:
+  String() noexcept;
+  String(size_t size, char character);
+  String(const char* str);
+  String(const String& other);
+  ~String();
+  String& operator=(const String& other);
+  void Clear();
+  void PushBack(char character);
+  void PopBack();
+  void Resize(size_t new_size);
+  void Resize(size_t new_size, char character);
+  void Reserve(size_t new_cap);
+  void ShrinkToFit();
+  void Swap(String& other);
+  char& operator[](size_t ind);
+  char operator[](size_t ind) const;
+  char& Front();
+  char Front() const;
+  char& Back();
+  char Back() const;
+  size_t Size() const;
+  bool Empty() const;
+  size_t Capacity() const;
+  char*& Data();
+  bool operator<(const String& other) const;
+  bool operator>(const String& other) const;
+  bool operator==(const String& other) const;
+  bool operator<=(const String& other) const;
+  bool operator>=(const String& other) const;
+  bool operator!=(const String& other) const;
+  String& operator+=(const String& other);
+  String operator+(String other);
+  String& operator*(unsigned int times);
+  std::vector<String> Split(const String& delim);
+  String Join(const std::vector<String>& strings);
 };
+
+std::ostream& operator<<(std::ostream& out, const String& str);
+std::istream& operator>>(std::istream& in, String& str);
