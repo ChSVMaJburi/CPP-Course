4db0aa2b82fe8bd1426bd4631148f0b4847ac816
Shahrom Aminov <aminov.aminovshakhrom@gmail.com>
2023-12-03 00:48:21 +0300

    solve of bigint

--- cpp-23-24-big-integer/cpp-23-24-big-integer.hpp
+++ cpp-23-24-big-integer/cpp-23-24-big-integer.hpp
@@ -1,6 +1,44 @@
 #pragma once
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
 #include <vector>
 
 class BigInt {
-    /* Not Implemented */
+ public:
+  BigInt();
+  BigInt(const int64_t& num);
+  BigInt(const std::string&);
+  BigInt(const BigInt& other);
+  ~BigInt();
+  bool operator==(const BigInt& other) const;
+  bool operator!=(const BigInt& other) const;
+  bool operator<(const BigInt& other) const;
+  bool operator<=(const BigInt& other) const;
+  bool operator>(const BigInt& other) const;
+  bool operator>=(const BigInt& other) const;
+  BigInt& operator=(const BigInt& other);
+  BigInt& operator-();
+  BigInt& operator+=(const BigInt& other);
+  BigInt operator+(const BigInt& other) const;
+  BigInt& operator-=(const BigInt& other);
+  BigInt operator-(const BigInt& other) const;
+  BigInt& operator*=(const BigInt& other);
+  BigInt operator*(const BigInt& other) const;
+  BigInt& operator/=(const BigInt& other);
+  BigInt operator/(const BigInt& other) const;
+  BigInt& operator++();
+  BigInt operator++(int);
+  BigInt& operator--();
+  BigInt operator--(int);
+ private:
+  void NotMinusZero();
+  std::pair<std::vector<short>, bool> GetDigits(int64_t num) const;
+  std::vector<short> digits_;
+  bool negative_ = 0;
+  const short kTen = 10;
+  const short kMinDigit = 0;
+  const short kMaxDigit = 9;
+  friend std::istream& operator>>(std::istream& in, BigInt& num);
+  friend std::ostream& operator<<(std::ostream& out, const BigInt& num);
 };
