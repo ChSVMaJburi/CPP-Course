8b28c7799c0f745f191d93d0dcc6fb341e61125c
Шахром Аминов <aminov.sh@phystech.edu>
2024-05-01 15:07:14 +0300

    fixed

--- cpp-23-24-matrix/cpp-23-24-matrix.hpp
+++ cpp-23-24-matrix/cpp-23-24-matrix.hpp
@@ -6,20 +6,19 @@
 template <size_t N, size_t M, typename T = int64_t>
 class Matrix {
  public:
-  Matrix() : data_(std::vector<std::vector<T>>(N, std::vector<T>(M, T()))) {}
+  Matrix() : data_(N, std::vector<T>(M)) {}
   Matrix(const std::vector<std::vector<T>>& other) : data_(other) {}
-  Matrix(const T& elem)
-      : data_(std::vector<std::vector<T>>(N, std::vector<T>(M, elem))) {}
+  Matrix(const T& elem) : data_(N, std::vector<T>(M, elem)) {}
 
   Matrix<N, M, T>& operator+=(const Matrix<N, M, T>& other);
 
   Matrix<N, M, T>& operator-=(const Matrix<N, M, T>& other);
   Matrix<N, M, T>& operator*=(const T& elem);
 
-  Matrix<M, N, T> Transposed();
+  Matrix<M, N, T> Transposed() const;
 
   T& operator()(size_t first_index, size_t second_index);
-  T operator()(size_t first_index, size_t second_index) const;
+  const T& operator()(size_t first_index, size_t second_index) const;
 
  private:
   std::vector<std::vector<T>> data_;
@@ -28,20 +27,19 @@ class Matrix {
 template <size_t N, typename T>
 class Matrix<N, N, T> {
  public:
-  Matrix() : data_(std::vector<std::vector<T>>(N, std::vector<T>(N, T()))) {}
+  Matrix() : data_(N, std::vector<T>(N)) {}
   Matrix(const std::vector<std::vector<T>>& other) : data_(other) {}
-  Matrix(const T& elem)
-      : data_(std::vector<std::vector<T>>(N, std::vector<T>(N, elem))) {}
+  Matrix(const T& elem) : data_(N, std::vector<T>(N, elem)) {}
 
   Matrix<N, N, T>& operator+=(const Matrix<N, N, T>& other);
   Matrix<N, N, T>& operator-=(const Matrix<N, N, T>& other);
   Matrix<N, N, T>& operator*=(const T& elem);
 
-  Matrix<N, N, T> Transposed();
+  Matrix<N, N, T> Transposed() const;
   T Trace() const;
 
   T& operator()(size_t first_index, size_t second_index);
-  T operator()(size_t first_index, size_t second_index) const;
+  const T& operator()(size_t first_index, size_t second_index) const;
 
  private:
   std::vector<std::vector<T>> data_;
@@ -78,8 +76,8 @@ Matrix<N, K, T> operator*(const Matrix<N, M, T>& first,
   Matrix<N, K, T> result;
   for (size_t i = 0; i < N; ++i) {
     for (size_t j = 0; j < K; ++j) {
-      T sum = 0;
-      for (size_t k = 0; k < M; ++k) {
+      T sum = first(i, 0) * second(0, j);
+      for (size_t k = 1; k < M; ++k) {
         sum += first(i, k) * second(k, j);
       }
       result(i, j) = sum;
@@ -116,7 +114,7 @@ Matrix<N, M, T>& Matrix<N, M, T>::operator*=(const T& elem) {
 }
 
 template <size_t N, size_t M, typename T>
-Matrix<M, N, T> Matrix<N, M, T>::Transposed() {
+Matrix<M, N, T> Matrix<N, M, T>::Transposed() const {
   Matrix<M, N, T> other(std::vector<std::vector<T>>(M, std::vector<T>(N)));
   for (size_t i = 0; i < N; ++i) {
     for (size_t j = 0; j < M; ++j) {
@@ -127,8 +125,8 @@ Matrix<M, N, T> Matrix<N, M, T>::Transposed() {
 }
 template <size_t N, typename T>
 T Matrix<N, N, T>::Trace() const {
-  T ans{};
-  for (size_t i = 0; i < N; ++i) {
+  T ans = data_[0][0];
+  for (size_t i = 1; i < N; ++i) {
     ans += data_[i][i];
   }
   return ans;
@@ -139,7 +137,8 @@ T& Matrix<N, M, T>::operator()(size_t first_index, size_t second_index) {
   return data_[first_index][second_index];
 }
 template <size_t N, size_t M, typename T>
-T Matrix<N, M, T>::operator()(size_t first_index, size_t second_index) const {
+const T& Matrix<N, M, T>::operator()(size_t first_index,
+                                     size_t second_index) const {
   return data_[first_index][second_index];
 }
 template <size_t N, typename T>
@@ -171,7 +170,7 @@ Matrix<N, N, T>& Matrix<N, N, T>::operator*=(const T& elem) {
 }
 
 template <size_t N, typename T>
-Matrix<N, N, T> Matrix<N, N, T>::Transposed() {
+Matrix<N, N, T> Matrix<N, N, T>::Transposed() const {
   Matrix<N, N, T> other(data_);
   for (size_t i = 0; i < N; ++i) {
     for (size_t j = 0; j < N; ++j) {
@@ -185,6 +184,7 @@ T& Matrix<N, N, T>::operator()(size_t first_index, size_t second_index) {
   return data_[first_index][second_index];
 }
 template <size_t N, typename T>
-T Matrix<N, N, T>::operator()(size_t first_index, size_t second_index) const {
+const T& Matrix<N, N, T>::operator()(size_t first_index,
+                                     size_t second_index) const {
   return data_[first_index][second_index];
 }
